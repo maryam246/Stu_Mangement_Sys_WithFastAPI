9c62d9e06f7fdf32b183947f4ae94f3fbd2ed27b
Maryam khanam <143519643+maryam246@users.noreply.github.com>
2024-11-08 19:10:50 +0500

    Add files via upload

---
+++ Student_Management_System/utils/auth.py
@@ -0,0 +1,43 @@
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from utils.config import admin_collection
+from utils.config import SECRET_KEY  # Import SECRET_KEY from config.py
+from utils.config import ALGORITHM  # Import ALGORITHM from config.py
+
+# Password hashing context
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+# Function to hash passwords
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+# Function to verify passwords
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+# Function to create a JWT token
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+# Function to decode JWT token
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload.get("sub")
+    except JWTError:
+        return None
+
+# Dependency to get the current logged-in admin
+async def get_current_admin(token: str = Depends(oauth2_scheme)):
+    email = verify_token(token)
+    if email is None:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    admin = admin_collection.find_one({"email": email})
+    if admin is None:
+        raise HTTPException(status_code=404, detail="Admin not found")
+    return admin
